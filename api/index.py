5db75d4fbb3b009ee4601f1cdc4efa7a73038715
dELCIo <64657972+dasandza@users.noreply.github.com>
2024-04-16 15:50:25 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
 
-@app.route('/about')
-def about():
-    return 'About'
+        #data = request.json
+        #from_ = unquote(data.get('From'))
+        #to = unquote(data.get('To'))
+        #message = unquote(data.get('Body'))
+        #date = unquote(data.get('Date'))
+        #message_id = unquote(data.get('MessageSID'))
+
+        # Process the data here
+        #log = getMessageLog(from_, to)
+        #responses = getResponse(message, log)
+        sleep(1)
+        return jsonify({'message': "webhook received"}), 200
+    else:
+        return jsonify({'error': 'Unsupported Media Type'}), 415
